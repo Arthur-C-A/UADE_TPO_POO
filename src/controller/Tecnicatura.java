ae61a5612807bdc087e51532e231674815ce52b6
Arthur <object.arthur@gmail.com>
2024-11-05 15:10:01 -0300

    asd

---
+++ src/controller/Tecnicatura.java
@@ -0,0 +1,33 @@
+package controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tecnicatura extends Carrera {
+    private final String[] materiasPropiasTecnicatura = {
+            "Testing de Aplicaciones",
+            "Optativa 1 (Marketing Digital)",
+            "Trabajo Integrador Final",
+            "Optativa 2 (Gestión de Proyectos)"
+    };
+
+    public Tecnicatura() {
+        super("Tecnicatura en Desarrollo de Software", 3,4);
+    }
+
+    @Override
+    public String[] getMaterias() {
+        List<String> todasLasMaterias = new ArrayList<>();
+        for (String materia : materiasGenerales) {
+            todasLasMaterias.add(materia);
+        }
+        for (String materia : getMateriasPropias()) {
+            todasLasMaterias.add(materia);
+        }
+        return todasLasMaterias.toArray(new String[0]);
+    }
+
+    public String[] getMateriasPropias() {
+        return materiasPropiasTecnicatura;
+    }
+}
