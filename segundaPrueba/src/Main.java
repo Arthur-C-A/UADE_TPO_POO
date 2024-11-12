7e2c49538fa4455abbceaef5ba874b5656e84a91
Arthur C. A. <159676770+Arthur-C-A@users.noreply.github.com>
2024-11-11 22:26:20 -0300

    Delete segundaPrueba directory

--- segundaPrueba/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        // Iniciar el sistema mostrando la ventana de login
-        LoginFrame loginFrame = new LoginFrame();
-        loginFrame.setVisible(true);
-    }
-}
